52f85aa93e601a649ee18cddd0fbaf491ea0291e
alextolkacov <aleksandrs.tolkacovs@cognizant.com>
2019-07-31 17:30:23 +0300

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
     return ( <
+        BrowserRouter >
+        <
         div className = 'app-wrapper' >
         <
         Header / >
         <
         Navbar / >
         <
-        Profile / >
+        div className = 'app-wrapper-content' >
         <
-        /div>
+        Route path = '/profile'
+        component = { Profile }
+        /> <
+        Route path = '/dialogs'
+        component = { Dialogs }
+        /> <
+        /div> <
+        /div> <
+        /BrowserRouter>
     );
 }
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,21 +2,17 @@ import React from 'react'
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
   return <div>My posts
     <div>
       <textarea></textarea>
       <button>Add post</button>
     </div>
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post message='Hi, how are you?'/>
+      <Post message='It is my first post'/>
     </div>
   </div>
 }
 
-export default MyPosts
+export default MyPosts  
